c143ae6407dd7a0bb48463610b1be85dd335da03
sandrugabriel <sandru11.gabriel@gmail.com>
2024-06-17 18:20:37 +0300

    done

---
+++ MasiniApi/Test/Car/Infrastucture/ApiWebApplicationFactory.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MasiniApi.Data;
+
+namespace Test.Car.Infrastucture
+{
+    public class ApiWebApplicationFactory : WebApplicationFactory<Program>
+    {
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+
+                var descr = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+
+                if (descr != null)
+                {
+                    services.Remove(descr);
+                }
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryAppDb");
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
+
+                using (var scope = services.BuildServiceProvider().CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<AppDbContext>();
+
+                    db.Database.EnsureCreated();
+                }
+            });
+        }
+
+
+
+    }
+}
---
+++ MasiniApi/Test/Car/IntegrationTests/CarIntegrationTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Text;
+using MasiniApi.Dto;
+using MasiniApi.Models;
+using Newtonsoft.Json;
+using Test.Car.Infrastucture;
+
+namespace Test.Car.IntegrationTests
+{
+    public class CarIntegrationTest : IClassFixture<ApiWebApplicationFactory>
+    {
+        private readonly HttpClient _httpClient;
+
+        public CarIntegrationTest(ApiWebApplicationFactory factory)
+        {
+            _httpClient = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Post_Create_ValidData_CreatedStatusCode()
+        {
+            var request = "/api/v1/car/createcar";
+            var car = new CreateCarRequest(){Marca = "test",AnulFacricatiei = 2000,Culoare = "test",Model = "test1"};
+
+            var content = new StringContent(JsonConvert.SerializeObject(car), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(request, content);
+            
+            Assert.Equal(HttpStatusCode.Created,response.StatusCode);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<Masini>(responseString);
+            
+            Assert.NotNull(result);
+            Assert.Equal(car.Culoare,result.Culoare);
+            Assert.Equal(car.AnulFacricatiei,result.Year);
+            Assert.Equal(car.Marca,result.Marca);
+
+        }
+
+        [Fact]
+        public async Task Post_Create_InvalidYear_AcceptedCode()
+        {
+            
+        }
+
+    }
+}
